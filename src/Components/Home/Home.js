af474325843f82867c37c27d42b993fb537a22d1
Enes AGIR <enes.agir.7@gmail.com>
2022-03-03 14:14:17 +0100

    slider and category

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,42 @@
+import { Carousel } from "bootstrap";
+import CarouselContainer from "./carouselContainer/CarouselContainer";
+import { Card } from "react-bootstrap";
+import './Home.css'
+
+import friut from './../../assets/category/friut.png';
+import vegatable from './../../assets/category/veg.png';
+import tol from './../../assets/category/tol.png';
+
 const Home = () => {
     return (
         <div>
-            <h1>This is the Home Page</h1>
+            <CarouselContainer />
+            <div className="category">
+                <Card className="cart">
+                    <Card.Img variant="top" src={friut}/>
+                    <Card.Body>
+                        <Card.Title>friut</Card.Title>
+                        
+                    </Card.Body>
+                </Card>
+                <Card className="cart" >
+                    <Card.Img className="img" variant="top" src={vegatable}/>
+                    <Card.Body>
+                        <Card.Title>vegatable</Card.Title>
+                        
+                    </Card.Body>
+                </Card>
+                <Card className="cart">
+                    <Card.Img variant="top" src={tol}/>
+                    <Card.Body>
+                        <Card.Title>toiletries</Card.Title>
+                        
+                    </Card.Body>
+                </Card>
+            </div>
+            
         </div>
+
     )
 }
 
