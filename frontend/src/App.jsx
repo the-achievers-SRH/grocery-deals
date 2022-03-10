ae9cf54acfd14b41523c1f49e5c17b17813dac71
Vivek Sengar <vk415072@gmail.com>
2022-03-10 12:32:42 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,32 +11,38 @@ import {Container} from 'react-bootstrap'
 import Products from "./Pages/Products/Products";
 
 function App() {
-    return (
-        <div className="App">
-  
-        <Header/>
-        <main className='py-0'>
-            <Container>
-            <h1>Welcome to Grocerilla</h1> 
-            <Routes>
-                <Route path='/' element={<Home/>} />
-                <Route path='/products' element={<ProductList/>} />
-                <Route path='/about' element={<About/>} />
-                <Route path='/login' element={<Login/>} />
-            </Routes>
-            </Container>
-        </main>
-        <Footer/>
+  return (
+    <div className="App">
 
-        {/* routes */}
-         
-  
-        {/* <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch> */}
-      </div>
-    );
-  }
-  
-  export default App;
-  
+      <Header/>
+      <main className='py-3'>
+        <Container>
+        <h1>Welcome to Grocerilla</h1>
+        </Container>
+      </main>
+
+      {/* navbar */}
+      <nav>
+        <h1>Grocery Deals Tracker</h1>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+
+      {/* routes */}
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/login' element={<Login/>} />
+        </Routes>
+
+      {/* <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch> */}
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
