f21b2b3a76c61cf0d28e50d0eb77f7811717825c
Vivek Sengar <vk415072@gmail.com>
2022-03-12 22:49:22 +0100

    express, seeder, models, data added

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+// db config/connection file
+import mongoose from  'mongoose'
+
+const connectDB = async () => {
+    try {
+        // await as mongoose returns a promise
+        // it takes mongoose URI and second, we have to pass some default options
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
+            // useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useCreateIndex: true,
+        })
+        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+        console.log(`Error: ${error.message}`.red.underline.bold)
+        process.exit(1)
+    }
+}
+
+export default connectDB
